32401b9ca3c5891fa4b065354f944672b3e79499
FredNWorth <ptshears@gmail.com>
2022-09-30 10:53:54 +0000

    added docker installation

--- ssh-nginx.sh
+++ ssh-nginx.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
 ssh ubuntu@18.130.204.3 <<EOF
-
-docker build -t -p 80:80 my-nginx nginx
-docker run my-nginx
+sudo apt update
+sudo apt install curl -y
+curl https://get.docker.com/ | sudo bash
+sudo usermod -aG docker $(whoami)
+sudo reboot
+docker build -t my-nginx nginx
+docker run --name my-nginx -d -p 80:80 my-nginx 
 
 echo "Yay, it ran!"
 EOF

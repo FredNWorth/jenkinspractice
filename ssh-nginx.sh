7f7cf2464428c72008d3d1339c8b920af5f285a4
FredNWorth <ptshears@gmail.com>
2022-09-30 10:56:25 +0000

    added jenkins as a user

--- ssh-nginx.sh
+++ ssh-nginx.sh
@@ -4,7 +4,7 @@ ssh ubuntu@18.130.204.3 <<EOF
 sudo apt update
 sudo apt install curl -y
 curl https://get.docker.com/ | sudo bash
-sudo usermod -aG docker $(whoami)
+sudo usermod -aG docker jenkins
 sudo reboot
 docker build -t my-nginx nginx
 docker run --name my-nginx -d -p 80:80 my-nginx 

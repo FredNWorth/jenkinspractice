5afb9f71b7f8e08e517379c421d4707b4f483de1
FredNWorth <ptshears@gmail.com>
2022-09-30 11:03:09 +0000

    got rid of the build and pulled instead

--- ssh-nginx.sh
+++ ssh-nginx.sh
@@ -4,9 +4,9 @@ ssh ubuntu@18.130.204.3 /bin/bash <<EOF
 sudo apt update
 sudo apt install curl -y
 curl https://get.docker.com/ | sudo bash
-sudo usermod -aG docker jenkins
-docker build -t my-nginx nginx
-docker run --name my-nginx -d -p 80:80 my-nginx 
+sudo usermod -aG docker ${whoami}
+docker pull nginx
+docker run --name my-nginx -d -p 80:80 nginx
 
 echo "Yay, it ran!"
 EOF

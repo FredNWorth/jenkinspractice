b64590b15dd340547cd4892fbe9d96bf9a95fe22
FredNWorth <ptshears@gmail.com>
2022-09-30 11:19:19 +0000

    added user manually

--- ssh-nginx.sh
+++ ssh-nginx.sh
@@ -4,7 +4,7 @@ ssh ubuntu@18.130.204.3 /bin/bash <<EOF
 sudo apt update
 sudo apt install curl -y
 curl https://get.docker.com/ | sudo bash
-sudo usermod -a -G docker ubuntu
+sudo usermod -aG docker ubuntu
 docker pull nginx
 docker run --name my-nginx -d -p 80:80 nginx
 

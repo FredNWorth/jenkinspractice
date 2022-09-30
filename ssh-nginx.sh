a1b7673d372510c1845e8d928efc2bee92632503
FredNWorth <ptshears@gmail.com>
2022-09-30 11:00:16 +0000

    removed reboot

--- ssh-nginx.sh
+++ ssh-nginx.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
-ssh ubuntu@18.130.204.3 <<EOF
+ssh ubuntu@18.130.204.3 /bin/bash <<EOF
 sudo apt update
 sudo apt install curl -y
 curl https://get.docker.com/ | sudo bash
 sudo usermod -aG docker jenkins
-sudo reboot
 docker build -t my-nginx nginx
 docker run --name my-nginx -d -p 80:80 my-nginx 
 
